649c44242cbbf79f9f96ce3ad71cedf8f73a257e
stasyukhymenko <yukhymenko.stas@gmail.com>
2024-11-06 16:43:21 +0200

    First task

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 const variantNumber = 98;
 const targetIndex = (variantNumber % 10) + 1;
 
+const firstElement = document.getElementById('firstElement');
+const secondElement = document.querySelector('#secondElement');
+
+let isFirstClicked = true;
+
+firstElement.addEventListener('click', function (e) {
+    if (isFirstClicked){
+        isFirstClicked = false;
+        firstElement.style.backgroundColor = 'lightblue';
+        firstElement.style.color = 'black';
+    }
+    else
+        swapColors(firstElement,secondElement);
+})
+
+let isSecondClicked = true;
+
+secondElement.addEventListener('click', function (e) {
+    if (isSecondClicked){
+        isSecondClicked = false;
+        secondElement.style.backgroundColor = 'coral';
+        secondElement.style.color = 'white';
+    }
+    else
+        swapColors(firstElement,secondElement);
+})
+
+function swapColors (firstElement, secondElement) {
+    const middleBackgroundColor = firstElement.style.backgroundColor;
+    const middleTextColor = firstElement.style.color;
+    firstElement.style.backgroundColor = secondElement.style.backgroundColor;
+    firstElement.style.color = secondElement.style.color;
+    secondElement.style.backgroundColor = middleBackgroundColor;
+    secondElement.style.color = middleTextColor;
+}
+
 const imageContainer = document.querySelector("a");
 
 function addImage() {
